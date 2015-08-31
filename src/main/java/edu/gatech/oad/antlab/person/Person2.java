0ee2b02ba50dcfbcbd81e7066fae9bf887c44c72
Jay Kamat <github@jgkamat.33mail.com>
2015-08-31 01:06:21 -0400

    Work on person2's method

--- src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -9,16 +9,16 @@ package edu.gatech.oad.antlab.person;
  * @version 1.1
  */
 public class Person2 {
-    /** Holds the persons real name */
-    private String name;
-	 	/**
+	/** Holds the persons real name */
+	private String name;
+	/**
 	 * The constructor, takes in the persons
 	 * name
 	 * @param pname the person's real name
 	 */
-	 public Person2(String pname) {
-	   name = pname;
-	 }
+	public Person2(String pname) {
+		name = pname;
+	}
 	/**
 	 * This method should take the string
 	 * input and return its characters in
@@ -30,8 +30,18 @@ public class Person2 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 2 put your implementation here
-	  return null;
+		// Use stringbuffers cause they are faster.
+		Random r = new Random();
+		StringBuffer ans = new StringBuffer();
+		StringBuffer in = new StringBuffer(input);
+
+		while (in.length() > 0) {
+			int tmp = r.nextInt(in.length());
+			ans.append(in.charAt(tmp));
+			in.removeCharAt(tmp);
+		}
+
+		return ans.toString();
 	}
 	/**
 	 * Return a string rep of this object
@@ -42,6 +52,6 @@ public class Person2 {
 	 *         object
 	 */
 	public String toString(String input) {
-	  return name + calc(input);
+		return name + calc(input);
 	}
 }
